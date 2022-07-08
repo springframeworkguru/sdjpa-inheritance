5264858bc456853330a07706c07274a0107c64bf
John Thompson <john@springframework.guru>
2022-07-08 13:09:47 -0400

    Initial add of project

---
+++ src/main/java/guru/springframework/sdjpainheritence/SdjpaInheritenceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sdjpainheritence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaInheritenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdjpaInheritenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/sdjpainheritence/SdjpaInheritenceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sdjpainheritence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaInheritenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
